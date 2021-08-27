bc307cb1e2bcb31bb0320ee14d08d86e91c895c0
ganes7777 <79572617+ganes7777@users.noreply.github.com>
2021-08-27 13:49:11 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,12 +1,25 @@
-# function to check if small string is 
-# there in big string
-def check(string, sub_str):
-    if (string.find(sub_str) == -1):
-        print("NO")
-    else:
-        print("YES")
+def second_largest(list):
+    maximum= max(list[0],list[1])
+    second_max= min(list[0],list[1])
+    n=len(list)
+    for i in range(2, n):
+        if list[i]>maximum:
+            second_max=maximum
+            maximum=list[i]
+        else:
+            if list[i]>second_max:
+                second_max=list[i]
             
-# driver code
-string = "saikiran"
-sub_str ="sai"
-check(string, sub_str)
+    return second_max
+
+
+#input of list
+li=[]
+n=int(input("Enter size of list "))
+for i in range(0,n):
+    e=int(input("Enter elements of list "))
+    li.append(e)
+
+#smallest
+print("second largest in ",li,"is",end=" ")
+print(second_largest(li))  
